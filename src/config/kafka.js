ac4ec2686bd0983d85594fb2cf67132f5aede70d
thebeo2004 <thenhuet2004@gmail.com>
2025-05-07 11:08:49 +0700

    caching branch - fix: fix Kafka url when containerizing worker

--- src/config/kafka.js
+++ src/config/kafka.js
@@ -5,9 +5,9 @@ export const OUTPUT_TOPIC = 'ocr-results-topic';
 
 const kafka_broker = process.env.KAFKA_BROKERS || "kafka:9092"
 
-const kafka = new Kafka({
+export const kafka = new Kafka({
     clientId: 'upload-app-producer',
-    brokers: ['kafka:9092']
+    brokers: [kafka_broker]
 });
 
 export const sending_msg =  async (msgPayload, producer) => {
