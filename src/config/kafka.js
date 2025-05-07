616c3b161804c7a79438e6883893372a5e2af6b1
thebeo2004 <thenhuet2004@gmail.com>
2025-05-07 23:56:22 +0700

    filter_scaling branch - feat: define unique clientID for KafkaClient among multiple worker container

--- src/config/kafka.js
+++ src/config/kafka.js
@@ -1,4 +1,5 @@
 import { Kafka } from "kafkajs";
+import crypto from 'crypto';
 
 export const RECEIVED_TOPIC = 'received_files';
 export const OUTPUT_TOPIC = 'ocr-results-topic';
@@ -6,7 +7,8 @@ export const OUTPUT_TOPIC = 'ocr-results-topic';
 const kafka_broker = process.env.KAFKA_BROKERS || "kafka:9092"
 
 export const kafka = new Kafka({
-    clientId: 'upload-app-producer',
+    // Define unique clientId among multiple workers
+    clientId: `ocr-worker-${crypto.randomBytes(4).toString('hex')}`,
     brokers: [kafka_broker]
 });
 
